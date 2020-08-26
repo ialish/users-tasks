7de31aafb09915aaa008a50d09e37ff64cbeb6d7
Eyal <eyal.shaar@gmail.com>
2020-08-26 23:35:50 +0300

    Updates

---
+++ precache-manifest.6aa00be8e874c7ff26e61ea6a0ddd367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec364f363c886716d7b8be80a3a6643",
+    "url": "/users-tasks/index.html"
+  },
+  {
+    "revision": "6f35cef9e515ccce5c4c",
+    "url": "/users-tasks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f078356e7664138d74f",
+    "url": "/users-tasks/static/js/2.01a348e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/users-tasks/static/js/2.01a348e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f35cef9e515ccce5c4c",
+    "url": "/users-tasks/static/js/main.52582184.chunk.js"
+  },
+  {
+    "revision": "20d7efb2c841925f809a",
+    "url": "/users-tasks/static/js/runtime-main.11e5568a.js"
+  }
+]);
