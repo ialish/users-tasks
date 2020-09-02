0ed33c85ef27e1a0448f5b2d10d31a8c46782b04
Eyal <eyal.shaar@gmail.com>
2020-09-02 22:04:17 +0300

    "selecting user" styling

--- src/components/User.js
+++ src/components/User.js
@@ -10,7 +10,8 @@ class User extends Component {
 			showOtherData: false,
 			todos: [],
 			isAllTodosCompleted: false,
-			button: ''
+			button: '',
+			highlightId: false
 		};
 	}
 
@@ -49,11 +50,15 @@ class User extends Component {
 
 	render() {
 		const { id, name, email, street, city, zipcode } = this.state.user;
-		const { showOtherData, isAllTodosCompleted } = this.state;
+		const { showOtherData, isAllTodosCompleted, highlightId } = this.state;
 
 		const styles = {
-			user: {
-				borderColor: `${isAllTodosCompleted ? 'green' : 'red'}`
+			userMain: {
+				borderColor: `${isAllTodosCompleted ? 'green' : 'red'}`,
+				backgroundColor: `${highlightId ? 'orange' : 'initial'}`
+			},
+			userDetails: {
+				display: `${highlightId ? 'block' : 'none'}`
 			},
 			otherData: {
 				display: `${showOtherData ? 'block' : 'none'}`
@@ -61,44 +66,57 @@ class User extends Component {
 		}
 
 		return (
-			<div className="user" style={styles.user}>
-				ID: {id}<br/>
-				<form onSubmit={this.handleSubmit}>
-					<label htmlFor={`name-${id}`}>Name:</label>
-					<input id={`name-${id}`} name="name" value={name}
-						onChange={this.handleChange} /><br/>
-					<label htmlFor={`email-${id}`}>Email:</label>
-					<input id={`email-${id}`} name="email" value={email}
-						onChange={this.handleChange} /><br/>
-	
-					<button
-						className="other-data-button"
-						type="button"
-						onMouseOver={() => this.setState({ showOtherData: true })}
-						onClick={() => this.setState({ showOtherData: false })}
-					>
-						Other Data
-					</button><br/>
-	
-					<div className="other-data" style={styles.otherData}>
-						<label htmlFor={`street-${id}`}>Street:</label>
-						<input id={`street-${id}`} name="street" value={street}
+			<div className="user">
+				<div className="user-main" style={styles.userMain}>
+					<label onClick={() => this.setState({ highlightId: !this.state.highlightId })}>
+						ID: {id}
+					</label><br/>
+					<form onSubmit={this.handleSubmit}>
+						<label htmlFor={`name-${id}`}>Name:</label>
+						<input id={`name-${id}`} name="name" value={name}
 							onChange={this.handleChange} /><br/>
-						<label htmlFor={`city-${id}`}>City:</label>
-						<input id={`city-${id}`} name="city" value={city}
+						<label htmlFor={`email-${id}`}>Email:</label>
+						<input id={`email-${id}`} name="email" value={email}
 							onChange={this.handleChange} /><br/>
-						<label htmlFor={`zipcode-${id}`}>Zip Code:</label>
-						<input id={`zipcode-${id}`} name="zipcode" value={zipcode}
-							onChange={this.handleChange} /><br/>
-					</div>
-	
-					<div className="update-delete-buttons">
-						<input className="update-button" type="submit" value="Update"
-							onClick={() => this.setState({ button: 'update' })} />
-						<input className="delete-button" type="submit" value="Delete"
-							onClick={() => this.setState({ button: 'delete' })} />
-					</div>
-				</form>
+		
+						<button
+							className="other-data-button"
+							type="button"
+							onMouseOver={() => this.setState({ showOtherData: true })}
+							onClick={() => this.setState({ showOtherData: false })}
+						>
+							Other Data
+						</button><br/>
+		
+						<div className="other-data" style={styles.otherData}>
+							<label htmlFor={`street-${id}`}>Street:</label>
+							<input id={`street-${id}`} name="street" value={street}
+								onChange={this.handleChange} /><br/>
+							<label htmlFor={`city-${id}`}>City:</label>
+							<input id={`city-${id}`} name="city" value={city}
+								onChange={this.handleChange} /><br/>
+							<label htmlFor={`zipcode-${id}`}>Zip Code:</label>
+							<input id={`zipcode-${id}`} name="zipcode" value={zipcode}
+								onChange={this.handleChange} /><br/>
+						</div>
+		
+						<div className="update-delete-buttons">
+							<input className="update-button" type="submit" value="Update"
+								onClick={() => this.setState({ button: 'update' })} />
+							<input className="delete-button" type="submit" value="Delete"
+								onClick={() => this.setState({ button: 'delete' })} />
+						</div>
+					</form>
+				</div>
+
+				<div className="user-details" style={styles.userDetails}>
+					<h1>This is another half of a page</h1>
+					<h1>This is another half of a page</h1>
+					<h1>This is another half of a page</h1>
+					<h1>This is another half of a page</h1>
+					<h1>This is another half of a page</h1>
+					<h1>This is another half of a page</h1>
+				</div>
 			</div>
 		);
 	}
